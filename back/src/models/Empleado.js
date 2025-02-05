dd2879c75622a86181ff450a912e52d0fdad2ec2
Raul <ras.inte@gmail.com>
2025-02-05 10:15:12 -0500

    Correciones al modelo

---
+++ back/src/models/Empleado.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('Empleado', {
+      id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      }
+      ,
+      nombre: {
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull: false,
+      }, 
+      tipo: {
+        type: DataTypes.ENUM('Vendedor', 'Coordinador'),
+        allowNull: false,
+        defaultValue: 'Vendedor'
+      },
+      correo:{
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      esVendedor: {
+        type: DataTypes.BOOLEAN
+      },
+      activoDesde: {
+        type: DataTypes.DATEONLY,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+      },
+      estado:{
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: true
+      }, 
+      borrado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+  
+  
+    },{ timestamps: false });
+  };
